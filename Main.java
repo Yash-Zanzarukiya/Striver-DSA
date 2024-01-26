5d7d2d5f371854533d5e56ece744ab14bb38290e
Yash-Zanzarukiya <zanzarukiyayash2711@gmail.com>
2024-01-27 02:48:08 +0530

    Pattern Question Solved

--- Main.java
+++ Main.java
@@ -1,16 +1,227 @@
 import java.util.*;
+
 public class Main {
 
-  public static void main(String[] args) {
-      // Write your code here
-      Scanner sc = new Scanner(System.in);
-      char ch = sc.nextLine().charAt(0);
+    public static void pattern0(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < n + i; j++) {
+                if (j > n - i && j < n + i)
+                    System.out.print("*" + " ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2 * n - i; j++) {
+                if (j >= i && j <= 2 * n - i)
+                    System.out.print("*" + " ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern2(int n) {
 
-      if(ch<'A' || ch>'z')
-          System.out.print("-1");
-      else if(ch<='a')
-          System.out.print("1");
-      else
-          System.out.print("0");
-  }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                if (i <= n / 2) {
+                    if (j > (n / 2) - i && j < (n / 2) + i)
+                        System.out.print("*" + " ");
+                    else
+                        System.out.print("  ");
+                } else {
+                    if ((j + n / 2) >= i && j <= n - (i - n / 2))
+                        System.out.print("*" + " ");
+                    else
+                        System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern3(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern4(int n) {
+        for(int i=0; i<n;i++){
+            for(int j=0; j<=i ;j++){
+                System.out.print("* ");
+            }
+                System.out.print("\n");
+        }
+    }
+    public static void pattern5(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern6(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern7(int n) {
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern8(int n) {
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern9(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=2*n-1;j++){
+                if(j>n-i && j<n+i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=2*n-1;j++){
+                if(j>=i && j<=2*n-i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void pattern10(int n) {
+        int star=1;
+        for(int i=1;i<=2*n;i++){
+            for(int j=1;j<=star;j++){
+                System.out.print('*');
+            }
+            if(i < n)
+                star++;
+            else
+                star--;
+            System.out.print('\n');
+        }
+    }
+
+    public static void pattern11(int n) {
+        for(int i=1; i<=n ;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((i+j+1)%2+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void pattern12(int n) {
+        for(int i=1; i<=n ;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j=1;j<=(n-1)*2;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern13(int n) {
+        int num=1;
+        for(int i=1; i<=n ;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+++" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern14(int n) {
+        for(int i=1; i<=n ;i++){
+            for(char ch='A';ch<'A'+i;ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern15(int n) {
+        for(int i=1; i<=n ;i++){
+            for(char ch='A';ch <= 'A'+n-i ;ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern16(int n) {
+        for(int i=1; i<=n ;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print((char)(int)('A'+i-1)+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void pattern17(int n) {
+        
+    }
+    public static void pattern18(int n) {
+
+    }
+    public static void pattern19(int n) {
+
+    }
+    public static void pattern20(int n) {
+
+    }
+    public static void pattern21(int n) {
+
+    }
+    public static void pattern22(int n) {
+
+    }
+    public static void pattern(int n) {
+        for(int i=1; i<=n ;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j=1;j<=(n-i)*2;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        pattern(n);
+    }
 }
